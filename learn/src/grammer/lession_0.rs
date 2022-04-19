0e9bde7eba50e3d0e024b94f3255d49f88c53bec
tukjax <tukjax@gmail.com>
2022-04-19 23:25:38 +0800

    feat add dependency on [ferris-says] add tuple example

--- learn/src/grammer/lession_0.rs
+++ learn/src/grammer/lession_0.rs
@@ -1,5 +1,19 @@
+use ferris_says::say; // from the previous step
+use std::io::{stdout, BufWriter};
+
 fn main() {
 
-// immutable const
-    println!("hello, rust");
+    let mut x = "  ";
+
+    let mut  x = 3;
+
+    println!("The value of x is: {}",x);
+
+
+
+    //e.g. tuple
+    let mut tupleTest:(u32,u32,u32)  = (1,2,3);
+
+    println!("{}",tupleTest.0)
+
 }
